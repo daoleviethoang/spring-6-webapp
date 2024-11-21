27205ab32b1a6cf5f04e09f7b57b38e2caf40165
daoleviethoang <daoleviethoang@gmail.com>
2024-11-21 15:15:33 +0700

    Initializing Data with Spring

--- src/main/java/guru/springframework/spring6webapp/repository/AuthorRepository.java
+++ src/main/java/guru/springframework/spring6webapp/repository/AuthorRepository.java
@@ -1,7 +1,7 @@
 package guru.springframework.spring6webapp.repository;
 
-import guru.springframework.spring6webapp.domain.Book;
+import guru.springframework.spring6webapp.domain.Author;
 import org.springframework.data.repository.CrudRepository;
 
-public interface AuthorRepository extends CrudRepository<Book, Long> {
+public interface AuthorRepository extends CrudRepository<Author, Long> {
 }
--- src/main/java/guru/springframework/spring6webapp/repository/BookRepository.java
+++ src/main/java/guru/springframework/spring6webapp/repository/BookRepository.java
@@ -1,7 +1,8 @@
 package guru.springframework.spring6webapp.repository;
 
 import guru.springframework.spring6webapp.domain.Author;
+import guru.springframework.spring6webapp.domain.Book;
 import org.springframework.data.repository.CrudRepository;
 
-public interface BookRepository extends CrudRepository<Author, Long> {
+public interface BookRepository extends CrudRepository<Book, Long> {
 }
